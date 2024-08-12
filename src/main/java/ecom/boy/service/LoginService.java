07fa770183cd168c5530463c15c8207cc7815959
Nrokap <Ping4453423@gmail.com>
2024-08-12 12:30:56 +0700

    edit spec 10d to 4d

--- src/main/java/ecom/boy/service/LoginService.java
+++ src/main/java/ecom/boy/service/LoginService.java
@@ -25,7 +25,7 @@ public class LoginService {
         List<Map<String, Object>> results = loginRepository.getAllUser();
         return  results.stream()
                 .map(t -> new ECBUserdto(
-                        String.format("%010d", (int) t.get("userid")),
+                        String.format("%04d", (int) t.get("userid")),
                         (String) t.get("username"),
                         (String) t.get("password"),
                         (String) t.get("name"),
@@ -41,7 +41,7 @@ public class LoginService {
         List<Map<String, Object>> results = loginRepository.getUserInfo();
         return  results.stream()
                 .map(t -> new ECBUsermgntdto(
-                        String.format("%010d", (int) t.get("userid")),
+                        String.format("%04d", (int) t.get("userid")),
                         (String) t.get("username"),
                         (String) t.get("password"),
                         (String) t.get("email")))
